241b8a2c73921c75b9cbf69749052e0849321152
Focal Fossa <69269521+FocalFossa0997@users.noreply.github.com>
2021-07-03 12:55:38 -0700

    Add files via upload

--- Wolf Editor.py
+++ Wolf Editor.py
@@ -5,16 +5,23 @@ import os
 import sys
 import codecs
 import io
-import time
-import threading
+from os import error, listdir
+from os.path import isfile, join
+
+versionCode = "1.6"
+
+if os.path.exists("../WolfEditorDocuments"):
+    pass
+else:
+    os.mkdir("../WolfEditorDocuments")
 
 from PySimpleGUI.PySimpleGUI import InputText, WIN_CLOSED, WIN_CLOSE_ATTEMPTED_EVENT, WIN_X_EVENT
 # Setup the main editor window
 print('Attempting to setup the window...')
-sg.theme('DarkGrey9')   # Add a touch of color
+sg.theme('SystemDefaultForReal')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Wolf Editor Desktop')],
-            [sg.Button('New'), sg.InputText(key='savePath'), sg.Button('Save'), sg.InputText(key='loadPath'), sg.Button('Load'), sg.Button('About'), sg.Button("Changelog"), sg.Button('Found a bug?'), sg.Button('Quit')],
+            [sg.Button('New'), sg.InputText(key='savePath'), sg.Button('Save'), sg.Checkbox("Save In Documents Folder", True, key='documentsFolder'),sg.InputText(key='loadPath'), sg.Button('Load'), sg.Button("Load Using Selector"), sg.Button('About'), sg.Button("Changelog"), sg.Button('Found a bug?'), sg.Button('Quit')],
             [sg.Multiline(size=(158, 42), key='MainDocument')]
 ]
 
@@ -44,7 +51,8 @@ while True:
         break
     
     if event == 'About': # of user clicks about:
-        alayout = [  [sg.Text('Wolf Editor Desktop is the offline, self-contained version of Wolf Editor.\nWolf editor is a simple text editor creted by Barry Piel (a.k.a Focal Fossa).\nYou are running version 1.5 Check the changelog.txt file for a list of all new changes.')],
+        alayout = [  [sg.Image("WolfEditor.gif")],
+            [sg.Text('Wolf Editor Desktop is the offline, self-contained version of Wolf Editor.\nWolf editor is a simple text editor creted by Barry Piel (a.k.a Focal Fossa).\nYou are running version ' + versionCode + ' Check the changelog.txt file for a list of all new changes.')],
             [sg.Button('Dismiss')]
         ] # Create our about window layout
 
@@ -98,40 +106,77 @@ while True:
         errorMessage('Found a bug?', 'Report it on the `issues` part Wolf Editor`s github page.')
     
     if event == 'Save':
-        filePathToSave = "../WolfEditorDocuments/" + values['savePath'] # There needs to be two \ characters, because python will freak out if only one \ is used.
-        
-        if filePathToSave == '../WolfEditorDocuments/':
-            errorMessage('Warning', 'You cannot save a file without specifying a file path.')
-        else:
-            if os.path.exists(filePathToSave):
-                ovlayout = [  [sg.Text('The file you are trying to save already exists. Do you want to replace it?')],
-            [sg.Button('No'), sg.Button('Yes')]
-        ] # Setup the layout for the iverwrite file conformation window
-
-                ovwindow = sg.Window('Make a new file?', ovlayout, icon='icon.ico') # Create the overwrite file conformation window
-
-                while True:
-                    
-                    ovevent, ovvalues = ovwindow.read()
-
-                    if ovevent == 'Yes': # When the user says yes to overwriting
-                        loaded_file = open(filePathToSave, 'w', encoding="utf8")
-                        loaded_file.write(values['MainDocument'])
-                        loaded_file.close()
-                        ovwindow.close()
-                        errorMessage('Wolf Editor', 'Successfully saved file')
-                        break
-                    if ovevent == sg.WIN_CLOSED or ovevent == 'No': # When the user says no to making a new file
-                        break
-                ovwindow.close() 
+        # Check if the user wanted to save the file in the WolfEditorDocuments folder
+        if values['documentsFolder'] == True:
+            filePathToSave = "../WolfEditorDocuments/" + values['savePath'] # There needs to be two \ characters, because python will freak out if only one \ is used.
             
+            if filePathToSave == '../WolfEditorDocuments/':
+                errorMessage('Warning', 'You cannot save a file without specifying a file path.')
             else:
-                print('The file does not exist, create new file before continue')
-                os.system('echo "" > ' + filePathToSave)
-                loaded_file = io.open(filePathToSave, 'w', encoding="utf8")
-                loaded_file.write(values['MainDocument'])
-                loaded_file.close()
-                errorMessage('Wolf Editor', 'Successfully saved file')
+                if os.path.exists(filePathToSave):
+                    ovlayout = [  [sg.Text('The file you are trying to save already exists. Do you want to replace it?')],
+                [sg.Button('No'), sg.Button('Yes')]
+            ] # Setup the layout for the iverwrite file conformation window
+
+                    ovwindow = sg.Window('Make a new file?', ovlayout, icon='icon.ico') # Create the overwrite file conformation window
+
+                    while True:
+                        
+                        ovevent, ovvalues = ovwindow.read()
+
+                        if ovevent == 'Yes': # When the user says yes to overwriting
+                            loaded_file = open(filePathToSave, 'w', encoding="utf8")
+                            loaded_file.write(values['MainDocument'])
+                            loaded_file.close()
+                            ovwindow.close()
+                            errorMessage('Wolf Editor', 'Successfully saved file')
+                            break
+                        if ovevent == sg.WIN_CLOSED or ovevent == 'No': # When the user says no to making a new file
+                            break
+                    ovwindow.close() 
+                
+                else:
+                    print('The file does not exist, create new file before continue')
+                    os.system('echo "" > ' + filePathToSave)
+                    loaded_file = io.open(filePathToSave, 'w', encoding="utf8")
+                    loaded_file.write(values['MainDocument'])
+                    loaded_file.close()
+                    errorMessage('Wolf Editor', 'Successfully saved file')
+        else:
+            filePathToSave = values['savePath'] # There needs to be two \ characters, because python will freak out if only one \ is used.
+            
+            if filePathToSave == '':
+                errorMessage('Warning', 'You cannot save a file without specifying a file path.')
+            else:
+                if os.path.exists(filePathToSave):
+                    ovlayout = [  [sg.Text('The file you are trying to save already exists. Do you want to replace it?')],
+                [sg.Button('No'), sg.Button('Yes')]
+            ] # Setup the layout for the iverwrite file conformation window
+
+                    ovwindow = sg.Window('Make a new file?', ovlayout, icon='icon.ico') # Create the overwrite file conformation window
+
+                    while True:
+                        
+                        ovevent, ovvalues = ovwindow.read()
+
+                        if ovevent == 'Yes': # When the user says yes to overwriting
+                            loaded_file = open(filePathToSave, 'w', encoding="utf8")
+                            loaded_file.write(values['MainDocument'])
+                            loaded_file.close()
+                            ovwindow.close()
+                            errorMessage('Wolf Editor', 'Successfully saved file')
+                            break
+                        if ovevent == sg.WIN_CLOSED or ovevent == 'No': # When the user says no to making a new file
+                            break
+                    ovwindow.close() 
+                
+                else:
+                    print('The file does not exist, create new file before continue')
+                    os.system('echo "" > ' + filePathToSave)
+                    loaded_file = io.open(filePathToSave, 'w', encoding="utf8")
+                    loaded_file.write(values['MainDocument'])
+                    loaded_file.close()
+                    errorMessage('Wolf Editor', 'Successfully saved file')
 
     if event == 'Load':
         filePathToLoad = "../WolfEditorDocuments/" + values['loadPath']
@@ -163,6 +208,47 @@ while True:
         
         else:
             errorMessage('Warning', 'The changelog file does not exist.')
+
+    if event == 'Load Using Selector':
+        files = [f for f in listdir('../WolfEditorDocuments') if isfile(join('../WolfEditorDocuments', f))]
+        lfusLayout = [
+            [sg.Listbox(files, key='_selectedFile_', size=(50, 15))],
+            [sg.Button("Load"), sg.Button("Cancel"), sg.Button("Refresh")]
+        ]
+        lfusWin = sg.Window('Load File', lfusLayout, icon='icon.ico') # Create the window
+
+        while True:
+            lfusEvent, lfusValues = lfusWin.read()
+            if lfusEvent == sg.WIN_CLOSED or lfusEvent == 'Cancel': # if user closes window or clicks dismiss in the about window
+                break
+            
+            if lfusEvent == 'Refresh': # if user closes window or clicks dismiss in the about window
+                files = [f for f in listdir('../WolfEditorDocuments') if isfile(join('../WolfEditorDocuments', f))]
+                lfusWin.FindElement('_selectedFile_').Update(values=files)
+            
+            if lfusEvent == 'Load': # if user closes window or clicks dismiss in the about windows
+                filePathToLoad = lfusWin.FindElement('_selectedFile_').get()
+                if len(filePathToLoad) == 0:
+                    errorMessage("Warning", "You cannot load a file without selecting one...")
+                else:
+                    filePathToLoad = filePathToLoad[0]
+                    print(filePathToLoad)
+                    filePathToLoad = "../WolfEditorDocuments/" + filePathToLoad
+                
+                    if filePathToLoad == '../WolfEditorDocuments/':
+                        errorMessage('Warning', 'You cannot load a file without selecting one...')
+                    else:
+                        if os.path.exists(filePathToLoad):
+                            loaded_file = io.open(filePathToLoad, 'r', encoding="utf8")
+                            loaded_file_data = loaded_file.read()
+                            window.FindElement('MainDocument').Update(loaded_file_data)
+                            #window.FindElement('savePath').Update(filePathToLoad)
+                            loaded_file.close()
+                            errorMessage('Wolf Editor', 'Successfully loaded file') 
+                            break       
+                        else:
+                            errorMessage('Warning', 'The file you`re trying to load does not exist.')        
+        lfusWin.close() # Close the about window
         
 
 window.close()
